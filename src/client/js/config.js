70e8386d065f84393ad68620347cae0a6604fe21
m-byte <m-byte@users.noreply.github.com>
2015-06-28 04:06:38 +0200

    Even more options and notes about those options

--- src/client/js/config.js
+++ src/client/js/config.js
@@ -1,10 +1,12 @@
 var config = {
-  server: "162.243.157.40:443",
-  secure: false,
-  masterServer: false,
-  skins: "./skins/",
-  logging: true,
-  colorLine: '#faa',
-  colorGrid: '#000',
-  colorBack: '#f1f1f1',
+  server: "162.243.157.40:443", // ip or domain and port of server
+  secure: false,                // true if using TLS
+  masterServer: false,          // server type: false => game server, true => master server
+  skins: "./skins/",            // skin directory
+  logging: true,                // false to hide log messages in developer tools
+  colorLine: '#faa',            // color for line (packet 21)
+  colorGrid: '#000',            // color of grid line
+  colorBack: '#f1f1f1',         // background color
+  gridSpacing: 50,              // spacing of grid lines
+  gridLine: 1,                  // thickness of a grid line
 };
